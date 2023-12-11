a38a6058022d382da7d88e148953f6751e8a0c6a
Subin Lebow <subinlebow@gmail.com>
2023-12-11 14:53:41 -0500

    section-divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between px-4 p-24">
         <Intro />
+        <SectionDivider />
     </main>
-  )
+  );
 }
