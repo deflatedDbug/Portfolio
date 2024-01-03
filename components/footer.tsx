e3105a815f331910361aba0fbeb9be7d6925dd8e
Subin Lebow <subinlebow@gmail.com>
2024-01-03 01:14:07 -0500

    updated footer and config files

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,14 +2,9 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-400">
-      <small className="mb-2 block text-xs">
+    <footer className="px-4 text-center text-gray-400">
+      <small className=" block text-xs">
         &copy; 2024 Subin Lebow. All rights reserved.
-        <p className="text-xs">
-          <span className="font-semibold">About this website:</span> {" "}
-          Built with React & Next.js, Typescript, Tailwind CSS, Framer Motion,
-          React Email & Resend.
-        </p>
       </small>
     </footer>
   );
